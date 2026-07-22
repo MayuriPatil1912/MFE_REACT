45d14c4ab7fe2b39e924640b7b3fb2150454c791
PRexpo <shambhu2729@gmail.com>
2026-07-22 16:21:37 +0530

    navigation links added

--- MFApp/src/components/Header.jsx
+++ MFApp/src/components/Header.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div className="header">
-            Microfrontend Host Application
-        </div>
-    )
-}
+  return (
+    <>
+      <div className="header">Microfrontend Host Application</div>
+
+      <ul className="nav">
+        {/* // if we use anchor tag instaed of Link page will reload so using Link */}
+
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/productlist"
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+          >
+            Todo List
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/foodlist"
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+          >
+            Food List
+          </NavLink>
+        </li>
+      </ul>
+    </>
+  );
+};
 
 export default Header;
