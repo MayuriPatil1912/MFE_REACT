d1c3c1a75630665604f75c632c35c60a4d483b1f
PRexpo <shambhu2729@gmail.com>
2026-07-22 16:01:54 +0530

    layout added

--- MFApp/src/App.jsx
+++ MFApp/src/App.jsx
@@ -1,35 +1,33 @@
 import React, { useState, useEffect, lazy, Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
 import FoodList from "./components/FoodList.jsx";
 import ProductList from "./components/ProductList.jsx";
 
 import "./style.css";
+import Layout from "./components/Layout.jsx";
 
 const App = () => {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <FoodList />,
-    },
-    {
-      path: "/foodlist",
-      element: <FoodList />,
-    },
-    {
-      path: "/productlist",
-      element: <ProductList />,
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <FoodList />,
+        },
+        {
+          path: "/foodlist",
+          element: <FoodList />,
+        },
+        {
+          path: "/productlist",
+          element: <ProductList />,
+        },
+      ],
     },
   ]);
-  return (
-    <RouterProvider router={router}>
-      <Header />
-      <FoodList />
-      <ProductList />
-      <Footer />
-    </RouterProvider>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 };
 
 export default App;
